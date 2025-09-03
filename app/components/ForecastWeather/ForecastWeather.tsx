3e6be792f767b04328ed4fab6dc11187484b52f6
elinaols <elol0031@student.hv.se>
2025-09-03 09:45:12 +0200

    refactor: use userInput or lastCity from localStorage for weather display

--- app/components/ForecastWeather/ForecastWeather.tsx
+++ app/components/ForecastWeather/ForecastWeather.tsx
@@ -52,7 +52,7 @@ export default function Forecast({city, userInput}: Props) {
             ) : (
                 <div className="w-[95%] px-[0.4rem] grid grid-cols-1 md:grid-cols-3 gap-6">
                     <Background weather={localStorage.getItem('lastWeather') || undefined}/>
-                    <h2 className="md:col-span-3 lg:text-4xl sm:text-3xl text-2xl py-[1rem]">Next 3 days weather in {userInput || forecastWeather?.location.name}</h2>
+                    <h2 className="md:col-span-3 lg:text-4xl sm:text-3xl text-2xl py-[1rem]">Next 3 days weather in {userInput || localStorage.getItem('lastCity')}</h2>
                     {/* 
                         Checks if the array exists and is not empty to ensure that the component doesn't attempt to iterate over an empty 
                         or non-existing array. Once the condition is fulfilled the map()-function will iterate over the fetched array.
