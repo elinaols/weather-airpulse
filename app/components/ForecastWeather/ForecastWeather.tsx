65366a1c6afe395f3e4c06e1d3a100cbbb1ace1a
elinaols <elol0031@student.hv.se>
2025-09-15 12:20:02 +0200

    fix: bug for error message

--- app/components/ForecastWeather/ForecastWeather.tsx
+++ app/components/ForecastWeather/ForecastWeather.tsx
@@ -27,6 +27,8 @@ export default function Forecast({city, userInput, lastCity, setWeatherCondition
                 const cleanCityStr = city.replace(/[åäÅÄ]/g, "a").replace(/[ö]/g, "o")
                 const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=e59095ee21d54915a00195257251501&q=${cleanCityStr}&days=14`)
                 
+                if (!response.ok) throw new Error("City not found")
+
                 const data: WeatherForecast = await response.json()
                 console.log(data)
 
