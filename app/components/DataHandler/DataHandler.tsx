12387cde9bbb96c8b3b90f08c0ebae43972f992b
elinaols <elol0031@student.hv.se>
2025-09-15 15:06:12 +0200

    fix dynamic variable for localStorage

--- app/components/DataHandler/DataHandler.tsx
+++ app/components/DataHandler/DataHandler.tsx
@@ -21,7 +21,7 @@ export default function DataHandler({showCurrentWeather, showForecastWeather}: P
 	// Contains the sanitized input from the user that views on the screen
 	const [userInput, setUserInput] = useState("")
 	const [lastCity, setLastCity] = useState<string | null>(null)
-	const [weatherCondition, setWeatherCondition] = useState<string>('Stockholm')
+	const [weatherCondition, setWeatherCondition] = useState<string>(() => localStorage.getItem('lastWeather') || "Sunny")
 	
 	// On initial render, retrieve the last searched city from localStorage and set it in state
 	useEffect(() => {
