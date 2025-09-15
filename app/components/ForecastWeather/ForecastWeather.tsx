89d138b2307266cc897fa27b79587c5c7c85e606
elinaols <elol0031@student.hv.se>
2025-09-15 12:57:38 +0200

    go back to previous

--- app/components/ForecastWeather/ForecastWeather.tsx
+++ app/components/ForecastWeather/ForecastWeather.tsx
@@ -64,7 +64,7 @@ export default function Forecast({city, userInput, lastCity, setWeatherCondition
                 <p>Loading...</p>
             ) : (
                 <div className="w-[95%] px-[0.4rem] grid grid-cols-1 md:grid-cols-3 gap-6">
-                    <h2 className="md:col-span-3 lg:text-4xl sm:text-3xl text-2xl py-[1rem]">Next 3 days weather in {error ? (lastCity || forecastWeather?.location.name) : (userInput || lastCity || forecastWeather?.location.name)}</h2>
+                    <h2 className="md:col-span-3 lg:text-4xl sm:text-3xl text-2xl py-[1rem]">Next 3 days weather in {userInput || lastCity || forecastWeather?.location.name}</h2>
                     {/* 
                         Checks if the array exists and is not empty to ensure that the component doesn't attempt to iterate over an empty 
                         or non-existing array. Once the condition is fulfilled the map()-function will iterate over the fetched array.
