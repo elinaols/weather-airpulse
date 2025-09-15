e0a68b56f3ba2cad80b006adf88cb1044fdba7c2
elinaols <elol0031@student.hv.se>
2025-09-15 14:05:33 +0200

    fix: preserve previous city and weather if API fetch fails

--- app/components/CurrentWeather/CurrentWeather.tsx
+++ app/components/CurrentWeather/CurrentWeather.tsx
@@ -42,13 +42,12 @@ export default function CurrentWeather({city, userInput, lastCity, setWeatherCon
 
 				if ("error" in data) throw new Error(data.error.message)
 
-				setCurrentWeather(data)
-				if (userInput) {
+				if (userInput.trim() && !error) {
+					setCurrentWeather(data)
 					localStorage.setItem('lastCity', userInput)
+					setWeatherCondition(data.current.condition.text)
+					localStorage.setItem('lastWeather', data.current.condition.text)				
 				}
-
-				setWeatherCondition(data.current.condition.text)
-				localStorage.setItem('lastWeather', data.current.condition.text)				
 			} catch (e) {
 				setError(e as Error)
 			} finally {
